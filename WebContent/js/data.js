e06f6f8691e6da4f141c826acaeddc35bb754685
zhangs <1729497919@qq.com>
2018-12-24 15:32:28 +0800

    web-抽奖小程序

---
+++ WebContent/js/data.js
@@ -0,0 +1,563 @@
+var personArray = new Array;
+
+    
+personArray.push({
+	id: 546,
+	image :"img/people/tx5.png",
+	thumb_image :"img/people/tx5.png",
+	name: "汤庆磊"
+});
+
+
+    
+personArray.push({
+	id: 722,
+	image :"img/people/穆洪军.jpg",
+	thumb_image :"img/people/穆洪军.jpg",
+	name: "穆洪军"
+});
+
+
+
+personArray.push({
+	id: 1044,
+	image :"img/people/刘莉莉.jpg",
+	thumb_image :"img/people/刘莉莉.jpg",
+	name: "刘莉莉"
+});
+
+
+
+personArray.push({
+	id: 1051,
+	image :"img/people/王建伟.jpg",
+	thumb_image :"img/people/王建伟.jpg",
+	name: "王建伟"
+});
+
+
+
+personArray.push({
+	id: 1711,
+	image :"img/people/张帅.jpg",
+	thumb_image :"img/people/张帅.jpg",
+	name: "张帅"
+});
+
+
+
+personArray.push({
+	id: 1717,
+	image :"img/people/郑磊.jpg",
+	thumb_image :"img/people/郑磊.jpg",
+	name: "郑磊"
+});
+
+
+
+personArray.push({
+	id: 1718,
+	image :"img/people/宋玉龙.jpg",
+	thumb_image :"img/people/宋玉龙.jpg",
+	name: "宋玉龙"
+});
+
+
+
+personArray.push({
+	id: 1730,
+	image :"img/people/张东瑞.jpg",
+	thumb_image :"img/people/张东瑞.jpg",
+	name: "张东瑞"
+});
+
+
+
+personArray.push({
+	id: 1732,
+	image :"img/people/陈岩.jpg",
+	thumb_image :"img/people/陈岩.jpg",
+	name: "陈岩"
+});
+
+
+
+personArray.push({
+	id: 1738,
+	image :"img/people/张家平.jpg",
+	thumb_image :"img/people/张家平.jpg",
+	name: "张家平"
+});
+
+
+
+personArray.push({
+	id: 1740,
+	image :"img/people/朱述平.jpg",
+	thumb_image :"img/people/朱述平.jpg",
+	name: "朱述平"
+});
+
+
+
+personArray.push({
+	id: 1816,
+	image :"img/people/王厚生.jpg",
+	thumb_image :"img/people/王厚生.jpg",
+	name: "王厚生"
+});
+
+
+
+personArray.push({
+	id: 1830,
+	image :"img/people/陈长会.jpg",
+	thumb_image :"img/people/陈长会.jpg",
+	name: "陈长会"
+});
+
+
+
+personArray.push({
+	id: 1832,
+	image :"img/people/张维作.jpg",
+	thumb_image :"img/people/张维作.jpg",
+	name: "张维作"
+});
+
+
+
+personArray.push({
+	id: 1879,
+	image :"img/people/王正德.jpg",
+	thumb_image :"img/people/王正德.jpg",
+	name: "王正德"
+});
+
+
+
+personArray.push({
+	id: 2272,
+	image :"img/people/张俊龙.jpg",
+	thumb_image :"img/people/张俊龙.jpg",
+	name: "张俊龙"
+});
+
+
+
+personArray.push({
+	id: 2733,
+	image :"img/people/高富堂.jpg",
+	thumb_image :"img/people/高富堂.jpg",
+	name: "高富堂"
+});
+
+
+
+personArray.push({
+	id: 2841,
+	image :"img/people/艾晶.jpg",
+	thumb_image :"img/people/艾晶.jpg",
+	name: "艾晶"
+});
+
+
+
+personArray.push({
+	id: 2865,
+	image :"img/people/杨君娜.jpg",
+	thumb_image :"img/people/杨君娜.jpg",
+	name: "杨君娜"
+});
+
+
+
+personArray.push({
+	id: 2875,
+	image :"img/people/王术庭.jpg",
+	thumb_image :"img/people/王术庭.jpg",
+	name: "王术庭"
+});
+
+
+
+personArray.push({
+	id: 11491,
+	image :"img/people/徐丙贵.jpg",
+	thumb_image :"img/people/徐丙贵.jpg",
+	name: "徐丙贵"
+});
+
+
+
+personArray.push({
+	id: 11492,
+	image :"img/people/史丰田.jpg",
+	thumb_image :"img/people/史丰田.jpg",
+	name: "史丰田"
+});
+
+
+
+personArray.push({
+	id: 11493,
+	image :"img/people/王玉文.jpg",
+	thumb_image :"img/people/王玉文.jpg",
+	name: "王玉文"
+});
+
+
+
+personArray.push({
+	id: 11523,
+	image :"img/people/甄洪鹏.jpg",
+	thumb_image :"img/people/甄洪鹏.jpg",
+	name: "甄洪鹏"
+});
+
+
+
+personArray.push({
+	id: 11567,
+	image :"img/people/郑习满.jpg",
+	thumb_image :"img/people/郑习满.jpg",
+	name: "郑习满"
+});
+
+
+
+personArray.push({
+	id: 11585,
+	image :"img/people/丁天.jpg",
+	thumb_image :"img/people/丁天.jpg",
+	name: "丁天"
+});
+
+
+
+personArray.push({
+	id: 11799,
+	image :"img/people/房洪臣.jpg",
+	thumb_image :"img/people/房洪臣.jpg",
+	name: "房洪臣"
+});
+
+
+
+personArray.push({
+	id: 11800,
+	image :"img/people/滕丽雯.jpg",
+	thumb_image :"img/people/滕丽雯.jpg",
+	name: "滕丽雯"
+});
+
+
+
+personArray.push({
+	id: 11812,
+	image :"img/people/周厚欣.jpg",
+	thumb_image :"img/people/周厚欣.jpg",
+	name: "周厚欣"
+});
+
+
+
+personArray.push({
+	id: 11850,
+	image :"img/people/胡修文.jpg",
+	thumb_image :"img/people/胡修文.jpg",
+	name: "胡修文"
+});
+
+
+
+personArray.push({
+	id: 11851,
+	image :"img/people/徐攀.jpg",
+	thumb_image :"img/people/徐攀.jpg",
+	name: "徐攀"
+});
+
+
+
+personArray.push({
+	id: 11852,
+	image :"img/people/赵静.jpg",
+	thumb_image :"img/people/赵静.jpg",
+	name: "赵静"
+});
+
+
+
+personArray.push({
+	id: 11860,
+	image :"img/people/丁洲.jpg",
+	thumb_image :"img/people/丁洲.jpg",
+	name: "丁洲"
+});
+
+
+
+personArray.push({
+	id: 11861,
+	image :"img/people/贾博.jpg",
+	thumb_image :"img/people/贾博.jpg",
+	name: "贾博"
+});
+
+
+
+personArray.push({
+	id: 11862,
+	image :"img/people/陈虎.jpg",
+	thumb_image :"img/people/陈虎.jpg",
+	name: "陈虎"
+});
+
+
+
+personArray.push({
+	id: 11863,
+	image :"img/people/徐道合.jpg",
+	thumb_image :"img/people/徐道合.jpg",
+	name: "徐道合"
+});
+
+
+
+personArray.push({
+	id: 11866,
+	image :"img/people/刘欢.jpg",
+	thumb_image :"img/people/刘欢.jpg",
+	name: "刘欢"
+});
+
+
+
+personArray.push({
+	id: 11885,
+	image :"img/people/胡雅丽.jpg",
+	thumb_image :"img/people/胡雅丽.jpg",
+	name: "胡雅丽"
+});
+
+
+
+personArray.push({
+	id: 11886,
+	image :"img/people/贾晓鹏.jpg",
+	thumb_image :"img/people/贾晓鹏.jpg",
+	name: "贾晓鹏"
+});
+
+
+
+personArray.push({
+	id: 11894,
+	image :"img/people/董行波.jpg",
+	thumb_image :"img/people/董行波.jpg",
+	name: "董行波"
+});
+
+
+
+personArray.push({
+	id: 12001,
+	image :"img/people/巨冲.jpg",
+	thumb_image :"img/people/巨冲.jpg",
+	name: "巨冲"
+});
+
+
+
+personArray.push({
+	id: 12033,
+	image :"img/people/徐小龙.jpg",
+	thumb_image :"img/people/徐小龙.jpg",
+	name: "徐小龙"
+});
+
+
+
+personArray.push({
+	id: 12042,
+	image :"img/people/于溪冰.jpg",
+	thumb_image :"img/people/于溪冰.jpg",
+	name: "于溪冰"
+});
+
+
+
+personArray.push({
+	id: 12045,
+	image :"img/people/孔维兵.jpg",
+	thumb_image :"img/people/孔维兵.jpg",
+	name: "孔维兵"
+});
+
+
+
+personArray.push({
+	id: 12049,
+	image :"img/people/周文政.jpg",
+	thumb_image :"img/people/周文政.jpg",
+	name: "周文政"
+});
+
+
+
+personArray.push({
+	id: 12058,
+	image :"img/people/王文洲.jpg",
+	thumb_image :"img/people/王文洲.jpg",
+	name: "王文洲"
+});
+
+
+
+personArray.push({
+	id: 12059,
+	image :"img/people/王伟.jpg",
+	thumb_image :"img/people/王伟.jpg",
+	name: "王伟"
+});
+
+
+
+personArray.push({
+	id: 12060,
+	image :"img/people/张允驰.jpg",
+	thumb_image :"img/people/张允驰.jpg",
+	name: "张允驰"
+});
+
+
+
+personArray.push({
+	id: 12061,
+	image :"img/people/胡玮.jpg",
+	thumb_image :"img/people/胡玮.jpg",
+	name: "胡玮"
+});
+
+
+
+personArray.push({
+	id: 12093,
+	image :"img/people/傅玉娇.jpg",
+	thumb_image :"img/people/傅玉娇.jpg",
+	name: "傅玉娇"
+});
+
+
+
+personArray.push({
+	id: 12095,
+	image :"img/people/白重庆.jpg",
+	thumb_image :"img/people/白重庆.jpg",
+	name: "白重庆"
+});
+
+
+
+personArray.push({
+	id: 12096,
+	image :"img/people/徐志响.jpg",
+	thumb_image :"img/people/徐志响.jpg",
+	name: "徐志响"
+});
+
+
+
+personArray.push({
+	id: 12104,
+	image :"img/people/韩广辉.jpg",
+	thumb_image :"img/people/韩广辉.jpg",
+	name: "韩广辉"
+});
+
+
+
+personArray.push({
+	id: 12107,
+	image :"img/people/胡玉飞.jpg",
+	thumb_image :"img/people/胡玉飞.jpg",
+	name: "胡玉飞"
+});
+
+
+
+personArray.push({
+	id: 12108,
+	image :"img/people/张帅_.jpg",
+	thumb_image :"img/people/张帅_.jpg",
+	name: "张帅"
+});
+
+
+
+personArray.push({
+	id: 12109,
+	image :"img/people/朱云铖.jpg",
+	thumb_image :"img/people/朱云铖.jpg",
+	name: "朱云铖"
+});
+
+
+    
+personArray.push({
+	id: 12109,
+	image :"img/people/李钰婧.jpg",
+	thumb_image :"img/people/李钰婧.jpg",
+	name: "李钰婧"
+});
+personArray.push({
+	id: 12110,
+	image :"img/people/于杰.jpg",
+	thumb_image :"img/people/于杰.jpg",
+	name: "于杰"
+});
+personArray.push({
+	id: 12111,
+	image :"img/people/庞玉帅.jpg",
+	thumb_image :"img/people/庞玉帅.jpg",
+	name: "庞玉帅"
+});
+personArray.push({
+	id: 12112,
+	image :"img/people/殷红梅.jpg",
+	thumb_image :"img/people/殷红梅.jpg",
+	name: "殷红梅"
+});
+personArray.push({
+	id: 12113,
+	image :"img/people/姚德臣.jpg",
+	thumb_image :"img/people/姚德臣.jpg",
+	name: "姚德臣"
+});
+personArray.push({
+	id: 12114,
+	image :"img/people/杨金钥.jpg",
+	thumb_image :"img/people/杨金钥.jpg",
+	name: "杨金钥"
+});
+personArray.push({
+	id: 12115,
+	image :"img/people/鲁飞.jpg",
+	thumb_image :"img/people/鲁飞.jpg",
+	name: "鲁飞"
+});
+personArray.push({
+	id: 12116,
+	image :"img/people/刘畅.jpg",
+	thumb_image :"img/people/刘畅.jpg",
+	name: "刘畅"
+});
+
+
+personArray.push({
+	id: 12116,
+	image :"img/people/王清明.jpg",
+	thumb_image :"img/people/王清明.jpg",
+	name: "王清明"
+});
